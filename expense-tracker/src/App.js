9bc8d17c338a5180f06d108a910c00129c085fae
Dev-using-Ansh <kuamrguptaansh0@gmail.com>
2024-11-20 15:43:46 +0530

    Added the form commpoent

---
+++ expense-tracker/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Form from './Form';
+function App() {
+  return (
+    <>
+    <h1 className='text-center text-xl font-bold mb-4'>Expense Tracker App</h1>
+    <Form />
+    </>
+  );
+}
+
+export default App;
