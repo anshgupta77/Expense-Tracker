f6792f2955ccb5fd4bdff96fd252168fd4933485
Dev-using-Ansh <kuamrguptaansh0@gmail.com>
2024-12-04 00:02:01 +0530

    change the LocalStorage.js

--- expense-tracker/src/services/localStorage.js
+++ expense-tracker/src/services/localStorage.js
@@ -1,19 +1,30 @@
 const EXPENSES_DATA_KEY = 'expenses_data_key';
-const sleep = (ms) => new Promise((resolve) => { 
-    setTimeout(resolve, ms)
+const sleep = (ms) => new Promise((resolve, reject) => { 
+    if(ms < 0){
+        reject(new Error("Time cant not be negative"));
+    }
+    else{
+        setTimeout(resolve, ms);
+    }
+
 })
 
 
 
 export async function getExpensesFromBackend() {
-    const expenseDataString = localStorage.getItem(EXPENSES_DATA_KEY) || "[]";
-    const expenses = JSON.parse(expenseDataString);
-    await sleep(4000);
-    return expenses;
+    try{
+        const expenseDataString = localStorage.getItem(EXPENSES_DATA_KEY) || "[]";
+        const expenses = JSON.parse(expenseDataString);
+        await sleep(-4000);
+        return expenses;
+
+    }catch(err){
+        return err;
+    }
 }
 
 export async function setExpensesInBackend(expenses) {
-    await sleep(4000);
+    await sleep(-4000);
     const updatedExpensesString = JSON.stringify(expenses);
     localStorage.setItem(EXPENSES_DATA_KEY, updatedExpensesString);
 }
